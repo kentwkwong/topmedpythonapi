39291680a4be4a48d09e6583e20c167680ad41a3
kentwkwong <kentwkwong@gmail.com>
2024-11-23 17:41:54 -0500

    test

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
-from service.email_service import sendemail
+from flask_cors import CORS
+from .service import email_service
 from pymongo import MongoClient
 
 app = Flask(__name__)
+CORS(app)
 
 # Email configuration
 SMTP_SERVER = "smtp.gmail.com"  # Change if you're using another service
@@ -24,7 +26,7 @@ def register():
 
 @app.route('/sendtimesheetemail', methods=['POST'])
 def sendtimesheetemail():
-    result = sendemail(request.json)
+    result = email_service.sendemail(request.json)
     return jsonify({"message":result.get("message")}), result.get("code")
 
 @app.route('/helloworld', methods=['GET'])
